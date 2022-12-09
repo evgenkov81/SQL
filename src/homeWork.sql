6c7fb81e527eee8f9da6ee075ed51a281044b396
evgenii <evgenkov81@gmail.com>
2022-12-09 15:34:59 +0300

    sql 2

--- src/homeWork.sql
+++ src/homeWork.sql
@@ -1,23 +1,24 @@
-create schema netology
+create table customers
+(
+    id           SERIAL PRIMARY KEY,
+    name         varchar not null,
+    surname      varchar not null,
+    age          int     not null,
+    phone_number varchar
+);
 
-    create table netology.persons(
-                                     name varchar not null,
-                                     surname varchar not null ,
-                                     age int not null ,
-                                     phone_number varchar,
-                                     city_of_living varchar,
-                                     primary key (name,surname,age)
-    );
+create table orders
+(
+    id           SERIAL PRIMARY KEY,
+    customer_id  integer,
+    date         DATE default now(),
+    product_name VARCHAR,
+    amount       integer,
+    FOREIGN KEY (customer_id) REFERENCES customers (id)
+);
 
-insert into netology.persons (name, surname, age, phone_number, city_of_living)
-VALUES ('Ivan', 'Ivanov', 20, '11111111111', 'Moscow'),
-       ('Petr', 'Petrov', 28, '22222222222', 'St-Petersburg'),
-       ('Vasiliy', 'Vasiliev', 29, '33333333333', 'Vladimir'),
-       ('Sergey', 'Sergeev', 26, '44444444444', 'Moscow')
+SELECT product_name
+FROM orders
+         JOIN customers c on c.id = orders.customer_id
+WHERE upper(c.name) = upper('alexey');
 
-
-select name, surname from netology.persons
-where city_of_living='Moscow'
-
-select name, surname, age, phone_number, city_of_living from netology.persons
-where age>27 order by age desc
